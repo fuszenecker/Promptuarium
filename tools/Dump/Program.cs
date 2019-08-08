4747d289a07b35f2d07e3061af95ff6214d3a1a2
Róbert Fuszenecker <robert.fuszenecker@betssongroup.com>
2019-08-08 17:04:35 +0200

    Added dumper.

---
+++ tools/Dump/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Promptuarium;
+
+namespace Dump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: Dump.exe <promptuarium_container>");
+                Environment.Exit(-1);
+            }
+
+            var container = Element.LoadAsync(args[0]).GetAwaiter().GetResult();
+            Console.WriteLine(container.TreeToString());
+        }
+    }
+}
